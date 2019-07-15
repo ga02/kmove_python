d64d4bb16e9684dba33a86cddcd07122ff1c8f91
ga02 <gy3397@gmail.com>
2019-07-15 17:45:57 +0900

    20190715 최종

--- basic/test.py
+++ basic/test.py
@@ -1,2 +1,13 @@
 print('hi')
 print("hi")
+food = "python's favorite food is \n\"maratang\""
+print(food)
+print('1','2','3',sep='-',end='')
+print('줄 안바뀐다')
+head='언제 마쳐'
+tail='남가영'
+num1=1995
+num2=2019
+print('i\'m %s%s hungry'%(tail,tail))
+print('열자리를 사용해서 소수점 이하 두자리만 출력했다! %10f'%4543.1257)
+print(f"내 이름은 {tail}이다. 나는 {num2-num1+1}살 이다 흑흑ㅠㅠ")
