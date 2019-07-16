cea806b31656bd5610b63a222f7cb5113a49b275
ga02 <gy3397@gmail.com>
2019-07-16 23:25:57 +0900

    20190716 집에서 수정

--- basic/lotto.py
+++ basic/lotto.py
@@ -2,6 +2,11 @@ import random
 list = []
 for i in range(1,6) :
     for j in range(1,7) :
-        n=0
-        while list.count(n) > 1 :
+        n = random.randint(1,45)
+        while list.count(n) > 0 :
             n = random.randint(1,45)
+        list.append(n)
+    list.sort()
+    print('로또 :',list)   
+    list.clear() 
+
